5caa44edd1a237ca6aaa286307786532c8b5f7ad
Denverjin <Denverjin@126.com>
2018-12-06 16:11:07 +0800

    Update

--- FastRead.cpp
+++ FastRead.cpp
@@ -2,10 +2,10 @@
 
 #define isdigit(c) (48 <= (c) && (c) <= 57)
 template <typename T> void read(T &x) {
-	x = 0; int b = 1, c = getchar();
-	for (;!isdigit(c);c = getchar()) if (c == '-') b = -b;
+	x = 0; int f = 1, c = getchar();
+	for (;!isdigit(c);c = getchar()) if (c == '-') f = -f;
 	for (; isdigit(c);c = getchar()) x = (x<<3) + (x<<1) + (c^48);
-	x *= b;
+	x *= f;
 }
 
 int main() {
